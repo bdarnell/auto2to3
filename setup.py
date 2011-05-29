5c3d65882daa567869da371a218066e7ca3d3eca
Ben Darnell <ben@bendarnell.com>
2011-05-29 14:29:15 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="auto2to3",
+      version="0.2",
+      author="Ben Darnell",
+      url="https://github.com/bdarnell/auto2to3/",
+      py_modules=["auto2to3"],
+      scripts=["bin/auto2to3"],
+      )
